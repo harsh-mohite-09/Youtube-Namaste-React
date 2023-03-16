5619be307353d719298970046d67f5cd5347abec
harsh-mohite-09 <harsh.mohite009@gmail.com>
2023-03-16 19:17:36 +0530

    initialing the App

---
+++ namaste-youtube/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-red-500">Harsh Mohite</h1>
+    </div>
+  );
+};
+
+export default App;
