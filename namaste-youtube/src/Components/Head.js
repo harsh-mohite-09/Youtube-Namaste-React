d529d0afb115d8dc2ba361d0250844092a6bf10b
harsh-mohite-09 <harsh.mohite009@gmail.com>
2023-03-21 08:39:27 +0530

    completed search suggestions feature

--- namaste-youtube/src/Components/Head.js
+++ namaste-youtube/src/Components/Head.js
@@ -5,9 +5,11 @@ import { SEARCH_API } from "../utils/config";
 
 const Head = () => {
   const [searchQuery, setSearchQuery] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [isSearchFocus, setIsSearchFocus] = useState(false);
 
   useEffect(() => {
-    const timer = setTimeout(() => getSearchSuggestions(), 200);
+    const timer = setTimeout(() => getSearchSuggestions(), 50);
 
     return () => {
       clearTimeout(timer);
@@ -17,10 +19,12 @@ const Head = () => {
   const getSearchSuggestions = async () => {
     const data = await fetch(SEARCH_API + searchQuery);
     const json = await data.json();
+    setSearchResult(json[1]);
     console.log(json[1]);
   };
 
   const dispatch = useDispatch();
+
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
   };
@@ -47,23 +51,46 @@ const Head = () => {
           </a>
         </div>
       </div>
-      <div className="flex items-center justify-center col-span-10">
-        <input
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          className="border border-gray-500 rounded-l-full h-10 w-1/3 px-4"
-          type="text"
-          autoFocus
-          placeholder="Search"
-        />
-        <button className="bg-gray-100 py-[0.4rem] pr-2 pl-1 h-10 rounded-r-full border border-l-0 border-gray-500 text-sm hover:bg-gray-200">
-          <img
-            className="h-full mx-4"
-            src="https://icones.pro/wp-content/uploads/2021/02/loupe-et-icone-de-recherche-de-couleur-grise.png"
-            alt="search"
-          />
-        </button>
+      <div className="flex justify-center items-center col-span-10">
+        <div className="flex flex-col w-1/2 relative">
+          <div className="flex w-full">
+            <input
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="border border-gray-500 rounded-l-full h-10 w-full px-4"
+              type="text"
+              autoFocus
+              onFocus={() => setIsSearchFocus(true)}
+              onBlur={() => setIsSearchFocus(false)}
+              placeholder="Search"
+            />
+            <button className="bg-gray-100 py-[0.4rem] pr-2 pl-1 h-10 rounded-r-full border border-l-0 border-gray-500 text-sm hover:bg-gray-200">
+              <img
+                className="h-full mx-4"
+                src="https://icones.pro/wp-content/uploads/2021/02/loupe-et-icone-de-recherche-de-couleur-grise.png"
+                alt="search"
+              />
+            </button>
+          </div>
+          {searchResult.length !== 0 && isSearchFocus && (
+            <div className="w-[88%] absolute top-14">
+              <ul className="ml-3 p-2 shadow-lg shadow-neutral-500 rounded-lg bg-white">
+                {searchResult.map((e) => {
+                  return (
+                    <li
+                      className="p-1 hover:bg-gray-100 cursor-pointer rounded-md"
+                      key={e}
+                    >
+                      🔍 {e}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
+
       <div className="col-span-1">
         <div className="flex justify-end items-center h-full">
           <img
